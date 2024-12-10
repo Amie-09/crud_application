6301e2f0ee7e6eef1b3edd8ebfc8caf58d92d5af
Amie-09 <amirdhini.baradharajan@gmail.com>
2024-12-10 13:25:09 +0530

    Initial commit

---
+++ crud_app/create_db.py
@@ -0,0 +1,9 @@
+# # create_db.py
+# from database import Base, engine
+
+# # Create all tables in the database
+# try:
+#     Base.metadata.create_all(bind=engine)
+#     print("Tables created successfully!")
+# except Exception as e:
+#     print(f"An error occurred: {e}")
