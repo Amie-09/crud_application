1b3081327ed5b55390a867dfc3476a79d129f106
Amie-09 <amirdhini.baradharajan@gmail.com>
2024-12-10 13:40:46 +0530

    deleted:    crud_app/__pycache__/database.cpython-312.pyc 	deleted:    crud_app/__pycache__/main.cpython-312.pyc 	deleted:    crud_app/__pycache__/models.cpython-312.pyc 	deleted:    crud_app/__pycache__/schemas.cpython-312.pyc 	deleted:    crud_app/__pycache__/splunk_logging.cpython-312.pyc 	deleted:    crud_app/alembic.ini 	deleted:    crud_app/alembic/README 	deleted:    crud_app/alembic/__pycache__/env.cpython-312.pyc 	deleted:    crud_app/alembic/env.py 	deleted:    crud_app/alembic/script.py.mako 	deleted:    crud_app/alembic/versions/70ca84099464_initial_migration.py 	deleted:    crud_app/alembic/versions/__pycache__/70ca84099464_initial_migration.cpython-312.pyc 	deleted:    crud_app/app.log 	deleted:    crud_app/create_db.py 	deleted:    crud_app/crud.py 	deleted:    crud_app/database.py 	deleted:    crud_app/main.py 	deleted:    crud_app/models.py 	deleted:    crud_app/requirements.txt 	deleted:    crud_app/routes/__pycache__/product.cpython-312.pyc 	deleted:    crud_app/routes/__pycache__/user.cpython-312.pyc 	deleted:    crud_app/routes/product.py 	deleted:    crud_app/routes/user.py 	deleted:    crud_app/schemas.py 	deleted:    crud_app/services/__pycache__/logging_service.cpython-312.pyc 	deleted:    crud_app/services/logging_service.py 	deleted:    crud_app/splunk_integration.py 	deleted:    crud_app/splunk_logging.py 	deleted:    splunk_app

--- crud_app/create_db.py
+++
@@ -1,9 +0,0 @@
-# # create_db.py
-# from database import Base, engine
-
-# # Create all tables in the database
-# try:
-#     Base.metadata.create_all(bind=engine)
-#     print("Tables created successfully!")
-# except Exception as e:
-#     print(f"An error occurred: {e}")
